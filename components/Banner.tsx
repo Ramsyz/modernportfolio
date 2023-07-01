fc33fa4bd8c18cb663dac5735964f3331b4a5a03
Ramsyz <seetaram.vannemreddi@gmail.com>
2023-07-01 05:27:52 -0400

    Changes in banner description

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -19,11 +19,9 @@ const Banner = () => {
           Front-End Developer
         </h3>
         <p className="text-base tracking-wide text-center md:text-left">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
-          modi id corporis eum, accusamus rem impedit ex obcaecati atque
-          aliquid, dolorem veniam commodi fuga incidunt odio vero eveniet
-          perferendis libero. Culpa quia quisquam repellat totam, accusantium
-          quibusdam dolorem dicta minima.
+          I'm a front-end web developer specializing in building(and
+          occasionally designing) exceptional digital experiences. Currently,
+          I'm focused on building responsive front-end web applications while learning back-end technologies.
         </p>
       </div>
     </div>
