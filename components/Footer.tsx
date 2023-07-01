7042e6cee4884609aaef9421a79b801967e7a278
Ramsyz <seetaram.vannemreddi@gmail.com>
2023-07-01 07:42:29 -0400

    change in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,5 @@
 const Footer = () => {
-  return (
-      <p className="text-sm py-2 text-center">
-        &copy; 2023 All rights reserved
-      </p>
-  );
+  return <p className="text-sm py-2 text-center">© 2023 All rights reserved</p>;
 };
 
 export default Footer;
